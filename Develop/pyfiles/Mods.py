c8cb9deb7ca9508b9fed4feeeb3479e7f5aef3e2
joshwoo70 <joshwoo69@hotmail.sg>
2016-09-19 20:24:19 +0800

    welp dis broke...

--- Develop/pyfiles/Mods.py
+++ Develop/pyfiles/Mods.py
@@ -5,7 +5,7 @@ import sys
 import logging
 import upsettings
 import SoundHandler as sound
-import jsoncfg
+import json_config
 import glob
 def resScan():
     logging.info('starting to scan for all resources.json files...')
@@ -13,15 +13,17 @@ def resScan():
         for names in files:
             if names == "resources.json":
                 resfile = os.path.join(root, names)
-                print(resfile)
-                logging.info('Resource file found: ' + resfile)
-                rescfg = jsontree.load_config(resfile)
-                if not soundlist:
+                Phrasingres(resfile)
+                logging.info('pushing job over to def...')
+    logging.info('added all modules.')
+def Phrasingres(file):
+                logging.info('got a job for me?')
+                config = json_config.connect(file)
+                if not config{MusicFiles}:
                     logging.info('sound list not given skipping...')
                 else:
                     logging.info('sound list is given. loading it.')
                     templist = []
-                    for x in soundlist:
+                    for x in config[MusicFiles]:
                         templist.append(x)
                     sound.genAlbowlist(templist)
-    logging.info('added all modules.')
