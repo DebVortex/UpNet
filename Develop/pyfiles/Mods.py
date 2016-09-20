009766e659de2f60d9ab03d2a344a8517ad784da
joshwoo70 <joshwoo69@hotmail.sg>
2016-09-20 21:02:37 +0800

    Did more work and patching.

--- Develop/pyfiles/Mods.py
+++ Develop/pyfiles/Mods.py
@@ -7,23 +7,27 @@ import upsettings
 import SoundHandler as sound
 import json_config
 import glob
+Modstempglob = []
 def resScan():
     logging.info('starting to scan for all resources.json files...')
     for root, dirs, files in os.walk('.\mods'):
         for names in files:
             if names == "resources.json":
+                root = root
+                print(root)
                 resfile = os.path.join(root, names)
-                Phrasingres(resfile)
+                Prasingres(resfile)
                 logging.info('pushing job over to def...')
+                sound.genAlbowlist(root,templist)
     logging.info('added all modules.')
-def Phrasingres(file):
-                logging.info('got a job for me?')
-                config = json_config.connect(file)
-                if not config{MusicFiles}:
-                    logging.info('sound list not given skipping...')
-                else:
-                    logging.info('sound list is given. loading it.')
-                    templist = []
-                    for x in config[MusicFiles]:
-                        templist.append(x)
-                    sound.genAlbowlist(templist)
+def Prasingres(root,file):
+    logging.info('got a job for me?')
+    config = json_config.connect(file)
+    if not (config["MusicFiles"]):
+        logging.info('sound list not given skipping...')
+    else:
+        logging.info('sound list is given. loading it.')
+        templist = []
+        for x in config["MusicFiles"]:
+            Modstempglob.append(root + x)
+            logging.info('adding :' + root + x)
