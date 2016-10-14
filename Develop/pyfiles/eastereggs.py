39c97e5ff8ef2a1bb0a7618ee4d64386b88cdad7
joshwoo70 <joshwoo69@hotmail.sg>
2016-10-14 17:56:10 +0800

    They are watching...
    Signed-off-by: joshwoo70 <joshwoo69@hotmail.sg>

--- Develop/pyfiles/eastereggs.py
+++ Develop/pyfiles/eastereggs.py
@@ -1,3 +1,9 @@
-
+import datetime
 import subprocess
 import time
+def timecheck():
+    if datetime.datetime.now().hour == '11':
+        if datetime.datetime.now().minute == '11':
+            print('Achievement GET!')
+            print('start UpNet at an ungodly hour.')
+            print('Double 11 will be watching over this whole session.')
